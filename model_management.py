d97c1986f3b411ae30178d9869d2c24ac29f166c
Tal Mekler <tal.mekler11@gmail.com>
2024-06-29 09:55:07 +0300

    REFACTOR: data members to private, change vars name

--- model_management.py
+++ model_management.py
@@ -1,50 +1,49 @@
-from typing import Type, Union
+from typing import Union, Optional
 
-from stable_baselines3 import DQN
-from stable_baselines3 import PPO
-
-from sumo_rl import SumoEnvironment
+from stable_baselines3 import DQN, PPO
 
 from config_manager import ConfigManager
 from project_logger import ProjectLogger
+
+from gymnasium import Env
 
 
 class ModelManagement:
 
     def __init__(self):
-        self.logger = ProjectLogger(self.__class__.__name__).setup_logger()
-        self.models_dict = {
+        self._logger = ProjectLogger(self.__class__.__name__).setup_logger()
+        self._models_dict = {
             "DQN": DQN,
             "PPO": PPO
         }
 
-    def save_model(self, model: Type[Union[DQN, PPO]], checkpoint_file_path: str) -> None:
+    def save_model(self, model: Union[DQN, PPO], checkpoint_file_path: str) -> None:
         try:
             model.save(checkpoint_file_path)
-            self.logger.info("Model checkpoint save successfully")
+            self._logger.info("Model checkpoint save successfully")
         except Exception as e:
-            self.logger.error(f"Failed to save model checkpoint: {e}")
+            self._logger.error(f"Failed to save model checkpoint: {e}")
 
-    def load_model(self, model_name: str, checkpoint_file_path: str) -> Type[Union[DQN, PPO, None]]:
-        model = self.models_dict.get(model_name)
-        if not model:
-            self.logger.error(f"Unsupported model: {model_name}")
+    def load_model(self, model_name: str, checkpoint_file_path: str) -> Optional[DQN, PPO]:
+        model_class = self._models_dict.get(model_name)
+        if not model_class:
+            self._logger.error(f"Unsupported model: {model_name}")
             return None
 
         try:
-            model.load(checkpoint_file_path)
-            self.logger.info("Model loaded successfully from checkpoint")
+            model = model_class.load(checkpoint_file_path)
+            self._logger.info("Model loaded successfully from checkpoint")
             return model
         except Exception as e:
-            self.logger.error(f"Failed to load model from checkpoint: {e}")
+            self._logger.error(f"Failed to load model from checkpoint: {e}")
             return None
 
-    def build_agent(self, model_name: str, env: Type[SumoEnvironment]) -> Type[Union[DQN, PPO, None]]:
+    def build_agent(self, model_name: str, env: Env) -> Optional[DQN, PPO]:
         agent_params = ConfigManager().load_agent_config(model_name, env)
         if not agent_params:
-            self.logger.error(f"Unsupported model: {model_name}")
+            self._logger.error(f"Unsupported model: {model_name}")
             return None
 
-        model = self.models_dict.get(model_name)(**agent_params)
-        self.logger.info(f"{model_name} agent built successfully")
+        model = self._models_dict.get(model_name)(**agent_params)
+        self._logger.info(f"{model_name} agent built successfully")
         return model
